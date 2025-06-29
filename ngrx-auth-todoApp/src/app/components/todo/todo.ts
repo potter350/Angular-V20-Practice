7d23f625412b3e3794abdf4f8dfb711637e0baab
potter350 <pandiyanpotter@gmail.com>
2025-06-29 19:41:03 +0530

    worked on todo ui and state

--- ngrx-auth-todoApp/src/app/components/todo/todo.ts
+++ ngrx-auth-todoApp/src/app/components/todo/todo.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as todoSelector from './../../todo/todo.selector'
+import * as todoAction from './../../todo/todo.actions'
+import { Todo as TodoModel } from '../../todo/todo.model';
 
 @Component({
   selector: 'app-todo',
@@ -7,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrl: './todo.css'
 })
 export class Todo {
+  private store = inject(Store)
+  todo$ : Observable<TodoModel[]> = this.store.select(todoSelector.selectAllTodo) 
+  isLoading$ : Observable<boolean | null> = this.store.select(todoSelector.selectIsTodoLoading)
+  isError$ : Observable<string | null> = this.store.select(todoSelector.selectTodoError)
+
+  newTask = ''
+  updatedTask = ''
+
+  ngOnInit(){
+    this.store.dispatch(todoAction.loadTodos())
+  }
+
+  //add new todo
+  
 
 }
